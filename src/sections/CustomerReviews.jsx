4009113d345e8ca52952fb4d3211a56818509026
Eric Kersh <kershner01@gmail.com>
2024-01-30 14:22:17 -0600

    pushed files

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,24 @@
+import { reviews } from "../constants"
+import ReviewCard from "../components/ReviewCard"
+
+const CustomerReviews = () => {
+  return (
+    <section>
+      <div className="max-container">
+        <h3 className="text-4xl text-center font-palanquin font-bold">What our<span className="text-coral-red"> Customers</span> Say?</h3>
+        <p
+        className="info-text m-auto mt-4 max-w-lg text-center"
+        >
+          Hear genuine stories from our satisfied customers about their
+          exceptional experiences with us.</p>
+          <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+            {reviews.map((review) => (
+              <ReviewCard key={review.customerName} { ...review }/>
+            ))}
+          </div>
+      </div>
+    </section>
+  )
+}
+
+export default CustomerReviews
---
+++ src/sections/index.js
@@ -0,0 +1,10 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe  from './Subscribe';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+
+export { Hero, CustomerReviews, Footer, PopularProducts, Services, SpecialOffers, Subscribe, SuperQuality }
